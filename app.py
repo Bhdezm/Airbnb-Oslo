2edf1227c1b8e5c457c77ebb26dc2fb820fa0b89
Bencomo Hernandez <122878569+Bhdezm@users.noreply.github.com>
2024-12-09 20:45:21 +0000

    final

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.impute import KNNImputer
 import plotly_express as px
 import numpy as np
+import streamlit.components.v1 as components
 import folium
 from folium.plugins import HeatMap
 from streamlit.components.v1 import html
@@ -30,68 +31,50 @@ st.markdown(
     <style>
     /* Fondo oscuro para toda la aplicación */
     .stApp {
-        background-color: #1e1e2f !important;  /* Color de fondo oscuro */
-        color: #ffffff !important;  /* Color de texto blanco */
-    }
-
-    /* Centrar y expandir contenido principal */
-    .main {
-        max-width: 1500px;
-        margin: 0 auto;
-        padding: 20px;
-    }
-
-    /* Barra lateral: Fondo oscuro y texto blanco */
-    .stSidebar {
-        background-color: #29293d !important; /* Fondo oscuro en la barra lateral */
-        color: #ffffff !important; /* Texto blanco en la barra lateral */
-    }
-
-    /* Títulos y subtítulos en blanco */
-    .stHeader, .stSubheader, .stTitle {
-        color: #ffffff !important;  /* Títulos en blanco */
-    }
-
-    /* Títulos, etiquetas y textos en blanco */
-    .stText, .stMarkdown, .stLabel, .stSelectbox, .stRadio, .stMultiselect {
-        color: #ffffff !important;  /* Texto blanco en todos los elementos */
-    }
-
-    /* Fondo oscuro para los botones */
-    .stButton>button {
-        background-color: #ff4b4b !important; /* Color de fondo del botón */
-        color: white !important; /* Texto blanco */
-        border: 1px solid #dcdcdc !important;  /* Borde blanco */
-    }
-
-    /* Campos de entrada (textos) con fondo oscuro y texto blanco */
-    .stTextInput>div>input, .stTextArea>div>textarea {
-        background-color: #29293d !important; /* Fondo oscuro en los campos de texto */
-        color: white !important;  /* Texto blanco en los campos de texto */
-    }
-
-    /* Seleccionadores desplegables con fondo oscuro y texto blanco */
-    .stSelectbox, .stRadio, .stMultiselect {
-        background-color: #29293d !important;  /* Fondo oscuro en los selectores */
-        color: white !important;  /* Texto blanco */
-    }
-
-    /* Barra lateral: Controles y texto dentro de la barra lateral */
-    .stSidebar .css-1l02zno {
-        background-color: #29293d !important; /* Fondo oscuro en la parte superior de la barra lateral */
+        background-color: #1e1e2f !important;  /* Fondo oscuro para toda la página */
         color: #ffffff !important;  /* Texto blanco */
     }
 
-    /* Color de borde en las gráficas */
-    .js-plotly-plot .plotly {
-        background-color: #29293d !important;  /* Fondo oscuro en los gráficos */
-        color: white !important;  /* Texto blanco en los gráficos */
+    /* Fondo y texto de la barra lateral */
+    .css-1d391kg, .css-1y4p8pa, .css-qbe2hs, .stSidebar {
+        background-color: #1e1e2f !important;  /* Fondo oscuro igual al resto */
+        color: #ffffff !important;  /* Texto blanco */
+    }
+
+    /* Texto de títulos y subtítulos */
+    .css-h3b2pw, h1, h2, h3, h4, h5, h6 {
+        color: #ffffff !important;  /* Encabezados en blanco */
+    }
+
+    /* Fondo oscuro y texto blanco en selectores desplegables */
+    .css-1l02zno, .stSelectbox, .stRadio, .stMultiselect {
+        background-color: #29293d !important;  /* Fondo oscuro */
+        color: #ffffff !important;  /* Texto blanco */
+    }
+
+    /* Bordes en los filtros desplegables */
+    .css-1d391kg select {
+        background-color: #29293d !important; /* Fondo oscuro */
+        color: white !important; /* Texto blanco */
+        border-color: #ffffff !important; /* Bordes blancos */
+    }
+
+    /* Fondo oscuro en los campos de texto */
+    .stTextInput>div>input, .stTextArea>div>textarea {
+        background-color: #29293d !important; /* Fondo oscuro */
+        color: #ffffff !important;  /* Texto blanco */
+    }
+
+    /* Fondo y texto de los botones */
+    .stButton>button {
+        background-color: #ff4b4b !important; /* Fondo rojo */
+        color: #ffffff !important; /* Texto blanco */
+        border: 1px solid #ffffff !important; /* Borde blanco */
     }
     </style>
     """,
     unsafe_allow_html=True
 )
-
 
 
 #FUNCION PARA CARGAR EL DATASET
@@ -139,10 +122,6 @@ if selected_superhost != "Todos":
 
 if selected_year != "Todos":
     filtered_data = filtered_data[filtered_data['year'] == selected_year]
-
-
-#st.image("Oslo.png", use_column_width=True, caption="Vista de Oslo")
-
 
 agg_data = df.groupby('neighbourhood').agg({
     'price': 'mean',
@@ -199,10 +178,6 @@ for _, row in agg_data.iterrows():
 #TITULO DE APLICACION
 st.title('Análisis de Airbnb en la ciudad de Oslo')
 
-#SIDEBAR
-#st.sidebar.image('airbnb.png', width=200)
-#st.sidebar.title('TITANIC')
-
 #TABS
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Introducción", "Oferta Airbnb", "Analisis Geográfico", 'Tasa de Ocupación','PowerBi','Conclusiones'])
 with tab1:
@@ -245,7 +220,7 @@ with col3:
 
 
 with tab2:
-    st.write('Evolución de la cantidad de Airbnb a lo largo de los años')
+    st.subheader('Evolución de la cantidad de Airbnb a lo largo de los años')
     evolution_per_year = df.groupby('year')['calculated_host_listings_count'].sum().reset_index()
 
     # Crear la figura
@@ -277,19 +252,37 @@ with tab2:
         plot_bgcolor="#1e1e2f",  # Fondo de la gráfica
         paper_bgcolor="#1e1e2f",  # Fondo fuera del área de la gráfica
         font=dict(color="white"),  # Color del texto
-        xaxis=dict(showgrid=False),  # Quitar líneas del grid en eje X
-        yaxis=dict(showgrid=False),  # Quitar líneas del grid en eje Y
+        xaxis=dict(
+        showgrid=False, 
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje X
+    ),
+    yaxis=dict(
+        showgrid=False,  
+    )
     )
 
     # Mostrar la gráfica en Streamlit
     st.plotly_chart(fig)
 
+    st.subheader("Reflexión sobre la Evolución de Airbnbs")
+    st.write("""
+    - El año **2016** marcó el pico más alto de nuevos Airbnbs en Oslo, indicando un crecimiento exponencial en la popularidad de la plataforma.
+    - A partir de **2020**, debido a la pandemia de COVID-19, se registró una drástica caída en la cantidad de Airbnbs, alcanzando su nivel más bajo desde 2014.
+    - En los años posteriores, se observa una **recuperación gradual**, aunque la cantidad actual de listados sigue estando por debajo del máximo alcanzado en 2016.
+    """)
 
-
+    # Detalles adicionales o reflexiones finales
+    st.info("""
+    Este análisis refleja cómo eventos globales, como la pandemia, pueden impactar drásticamente el mercado de alquileres a corto plazo.
+    Sin embargo, la tendencia de recuperación sugiere que la confianza en plataformas como Airbnb está retornando.
+    """)
 
 with tab3:
-    st.write('Analisis geográfico')
+    st.subheader('Análisis geográfico')
+
     st_folium(mapa, width=1000)
+
+    st.subheader('Precio Medio por vecindario')
 
     neighbourhood_prices = df.groupby('neighbourhood')['price'].mean().reset_index().sort_values('price', ascending=False)
 
@@ -297,7 +290,6 @@ with tab3:
     # Crear el gráfico de barras con Plotly Express
     fig = px.bar(
         neighbourhood_prices,
-        title='Precio medio por vecindario',
         x='neighbourhood',
         y='price',
         labels={'neighbourhood': 'Vecindario', 'price': 'Precio Medio ($)'},
@@ -310,8 +302,7 @@ with tab3:
         xaxis_tickangle=-45,
         plot_bgcolor='#1e1e2f',  # Fondo del área del gráfico
         paper_bgcolor='#1e1e2f',  # Fondo general del gráfico
-        font=dict(color='white'),  # Color del texto
-        title_font=dict(size=20),  # Tamaño del título
+        font=dict(color='white'),  # Color del texto  # Tamaño del título
         xaxis=dict(showgrid=False),  # Ocultar líneas del grid en eje X
         yaxis=dict(showgrid=False)   # Ocultar líneas del grid en eje Y
     )
@@ -322,7 +313,6 @@ with tab3:
     
 
 with tab4:
-    st.write('Tasa de ocupación')
     fig = px.scatter(filtered_data, x='price', y='occupancy_rate', 
                 color='neighbourhood', 
                 labels={'price': 'Precio', 'occupancy_rate': 'Tasa de Ocupación (%)'})
@@ -331,8 +321,19 @@ with tab4:
     plot_bgcolor='#1e1e2f',  # Fondo del gráfico
     paper_bgcolor='#1e1e2f',  # Fondo general
     font_color='white',  # Color del texto  # Estilo del título
-    xaxis=dict(gridcolor='#444444'),  # Color de las líneas del grid (opcional)
-    yaxis=dict(gridcolor='#444444')  # Color de las líneas del grid (opcional)
+    xaxis=dict(
+        showgrid=False, 
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje X
+    ),
+    yaxis=dict(
+        showgrid=True,  # Mostrar grid en eje Y
+        gridcolor='gray',
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje Y
+    ),
+    legend=dict(
+        title=dict(font=dict(color='white')),  # Color del título de la leyenda
+        font=dict(color='white')  # Color del texto de la leyenda
+    )  # Color de las líneas del grid (opcional)
 )
 
 # Mostrar el gráfico en Streamlit
@@ -342,15 +343,25 @@ with tab4:
 
     st.write('Tasa de ocupación por diferenciado en superhost y regularhost')
     fig = px.box(filtered_data, x='host_is_superhost', y='occupancy_rate', 
-            title='Tasa de Ocupación: Superhost vs Host Regular', 
             labels={'superhost': 'Tipo de Host', 'occupancy_rate': 'Tasa de Ocupación (%)'})
 
     fig.update_layout(
     plot_bgcolor='#1e1e2f',  # Fondo del gráfico
     paper_bgcolor='#1e1e2f',  # Fondo general
     font_color='white',
-    xaxis=dict(gridcolor='#444444'),  # Color de las líneas del grid (opcional)
-    yaxis=dict(gridcolor='#444444')  # Color del texto  # Estilo del título
+    xaxis=dict(
+        showgrid=False, 
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje X
+    ),
+    yaxis=dict(
+        showgrid=True,  # Mostrar grid en eje Y
+        gridcolor='gray',
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje Y
+    ),
+    legend=dict(
+        title=dict(font=dict(color='white')),  # Color del título de la leyenda
+        font=dict(color='white')  # Color del texto de la leyenda
+    )  # Color del texto  # Estilo del título
 )
 
 # Mostrar el gráfico en Streamlit
@@ -358,8 +369,91 @@ with tab4:
     st.write("Este gráfico muestra la relación entre el precio y la tasa de ocupación, diferenciados por vecindarios.")
     st.plotly_chart(fig)
 
+    grouped_data = filtered_data.groupby(['neighbourhood', 'host_is_superhost'], as_index=False).agg({'price': 'mean'})
+
+    st.subheader('Precio medio por Superhost')
+# Crear gráfico de barras apiladas
+    fig = px.bar(
+    grouped_data,
+    x='neighbourhood',
+    y='price',
+    color='host_is_superhost', 
+    labels={
+        'neighbourhood': 'Vecindario',
+        'price': 'Precio Medio ($)',
+        'host_is_superhost': 'Tipo de Anfitrión'
+    },
+    color_discrete_map={
+        't': 'blue', 
+        'f': 'orange'  
+    }
+    )
+
+# Ajustar la rotación de los ticks del eje x y diseño
+    fig.update_layout(
+    xaxis_tickangle=-45, 
+    plot_bgcolor='#1e1e2f',  
+    paper_bgcolor='#1e1e2f',
+    font=dict(color='white'),  
+    xaxis=dict(
+        showgrid=False, 
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje X
+    ),
+    yaxis=dict(
+        showgrid=True,  # Mostrar grid en eje Y
+        gridcolor='gray',
+        tickfont=dict(color='white')  # Color de las etiquetas en el eje Y
+    ),
+    legend=dict(
+        title=dict(font=dict(color='white')),  # Color del título de la leyenda
+        font=dict(color='white')  # Color del texto de la leyenda
+    )
+    )
+
+# Mostrar en Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
 with tab5:
-    st.write('Power Bi')
+    st.subheader("Informe de Power BI - Airbnb Oslo")
+    
+    # Agregar iframe para incrustar el informe de Power BI
+    st.markdown(
+        """
+        <iframe 
+            title="Power BI Report" 
+            width="100%" 
+            height="800" 
+            src="https://app.powerbi.com/view?r=eyJrIjoiYzM1NGQ3M2YtMDJhYi00ZTM2LTlhNzQtYjJkN2VlYzFjN2RmIiwidCI6IjhhZWJkZGI2LTM0MTgtNDNhMS1hMjU1LWI5NjQxODZlY2M2NCIsImMiOjl9" 
+            frameborder="0" 
+            allowFullScreen="true"></iframe>
+        """,
+        unsafe_allow_html=True
+    )
 
 with tab6:
-    st.write('Conclusiones')
+    st.subheader("Precio y Vecindarios")
+    st.write("""
+        - Los vecindarios más caros se concentran al Oeste de Oslo, destacando **Nordstrand**, **Vestre Aker** y **Ullem**.
+        - En comparación, vecindarios como **Stovner** o **Grorud** ofrecen precios más accesibles, atrayendo viajeros con menor presupuesto.
+    """)
+
+    # Impacto de los Superhosts
+    st.subheader("Impacto de los Superhosts")
+    st.write("""
+        - Los superhosts tienden a cobrar un precio medio mayor en comparación con los anfitriones regulares.
+        - Este aumento de precio puede justificarse por las **mejores reseñas** y la mayor confianza percibida por los usuarios a la hora elegir estancia.
+    """)
+
+    # Tasa de Ocupación
+    st.subheader("Tasa de Ocupación y Disponibilidad")
+    st.write("""
+        - La tasa de ocupación más alta se observa en las zonas de **Grorud** y **Gamle Oslo**, situandose en la zona centro y a las afueras.
+        - Estas Areas más baratas con una alta ocupación, sugieren oportunidades, ya que proporcionan una alta confianza en cuanto a la estancia.
+    """)
+
+    # Recomendaciones
+    st.subheader("Recomendaciones")
+    st.write("""
+        - Para propietarios: Aumentar las reseñas, ofrecer servicios adicionales y convertirse en superhost puede aumentar el ingreso promedio y la tasa de ocupación.
+        - Para viajeros: Explorar vecindarios menos hacia las afueras como **Grorud** podría ofrecer una experiencia más auténtica a menor costo.
+    """)
